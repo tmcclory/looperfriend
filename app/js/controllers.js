2c8e766c1d423bdcb13a1daa02331744eb1e78f6
unknown <Tim@.(none)>
2013-06-11 01:57:51 -0400

    Ranged tables.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,42 @@
 
 /* Controllers */
 
+
+function TrackController($scope) {
+	function newTrack(i,j) {
+		if(typeof(i)==='undefined') {i = 16;}
+		if(typeof(j)==='undefined') {j = 16;}
+		return {
+			'i': i,
+			'j': j,
+			'pattern' : []
+		};
+	}
+	var t = newTrack();
+	$scope.tracks = [t];
+}
+
+
+
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }]).
+  controller('TrackController',[function($scope) {		
+		function newTrack(i,j) {
+			if(typeof(i)==='undefined') {i = 16;}
+			if(typeof(j)==='undefined') {j = 16;}
+			return {
+				'i': i,
+				'j': j,
+				'pattern' : []
+			};
+		}
+		var t = newTrack();
+		$scope.tracks = [t];
+	}
+  
+  ]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,14 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).
+  filter('range', function() {
+		return function(input,total) {
+			var i=0;
+			total = parseInt(total,10);
+			for ( i=0; i<total; i+=1) {
+				input.push(i);
+			} 
+			return input;
+		};
+  });
